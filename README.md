434cb10bb2ececf8b8876d75b3ff0886c448f1cc
klmichael <87207684+klmichael@users.noreply.github.com>
2021-09-24 13:48:42 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,46 @@
+# Project Title: Eye Exercise: A Monster Makeover
+
+## Description of Project: 
+This project started with an assignment from Professor John Williams in my MIT XPro Bootcamp in week 8.  We were given the files constructing an eye in the browser that appears to follow the mouse movement and asked to add a second eye.
+
+After doing this, I extended the assignment and converted it into a game where the user gets to interact with a pair of eyes (the "monster") in several ways.  They can change the size and shape of the eyes; turn the watching function on and off; make the monster blink; add irises to the pupils and repeatedly change their color to a random color; and use the provided 6 "eye shadows" (the "makeover").
+
+*Original Author:* John Williams
+*Additional Coding:* Kathryn Michael
+
+## How to Run:
+
+Download these three files and save them itogether in a directory:
+-index.html
+-style.css
+-eyes.js
+
+Open index.html in your browser. You should now be able to play the game in your browser.
+
+## Roadmap of Future Improvements:
+
+In the future, I would like to add a **mouth** that would add to the user interaction and animation.  I would also like to add a **flying insect** that moves across the screen randomly and is watched by the eyes.
+
+## License Information:
+
+**MIT License**
+
+*Copyright (c) 2020 John Williams*
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ eyes.js
@@ -0,0 +1,114 @@
+    //This balls variable was given to us.  I added the rest of the variables to support additional functions.
+    var balls = document.getElementsByClassName("ball");
+    var bothEyes = document.getElementsByClassName("eye");
+    var watch = false;
+    var eyeShadows= [
+      "hotpink",
+      "plum",
+      "cyan",
+      "gold",
+      "lemonchiffon",
+      "chartreuse"
+    ];
+    var eyeShadow = eyeShadows[0];
+
+    //This script was given to us, but I modified it.
+    //Initially it was set to listen for mouse movement once the page loaded.
+    //I changed it to be a function that had to be called ("watchingUser" instead of "document.onmousemove").
+    //I also added the "- 50" to var y because I wanted the pupils to be able to look up more.
+    watchingUser = () => {
+      var x = (event.clientX * 100) / window.innerWidth + "%";
+      var y = ((event.clientY * 100) / window.innerHeight) - 50 + "%";
+      for (let i = 0; i < 2; i++) {
+        balls[i].style.left = x;
+        balls[i].style.top = y;
+      }
+    };
+    
+    //I added these three functions to give the user control over whether or not the eyes are watching.
+    function onWatch() {
+      document.addEventListener('mousemove', watchingUser);
+      watch = true;
+    }
+
+    function offWatch() {
+      document.removeEventListener('mousemove', watchingUser);
+      watch = false;
+    }
+
+    function toggleWatch() {
+      watch ? offWatch() : onWatch();
+    }
+
+    //I added these two functions so that the end-user could switch between the initial shape of the eyes (calm) and a rounder shape representing "surprised" eyes.
+    surpriseEyes = () => {
+      for (let i = 0; i < 2; i++) {
+        bothEyes[i].style.height = "200px";
+      }
+    }
+
+    calmEyes = () => {
+      for (let i = 0; i < 2; i++) {
+        bothEyes[i].style.height = "120px";
+      }
+    };
+
+    //I added a hotpink top border to the styles.css document for class eye to represent a top eyelid.
+    //I then wrote this function to make it appear as if the eyes blinked at the end-user.
+    //A later function gives the user the ability to change the eyeshadow.
+    blink = () => {
+      for (let i = 0; i < 2; i++) {
+        balls[i].style.borderWidth = "0px";
+        balls[i].style.backgroundColor = eyeShadow;
+        bothEyes[i].style.borderWidth = "0px";
+        bothEyes[i].style.backgroundColor = eyeShadow;
+      }
+
+      setTimeout(() => {
+        for (let i = 0; i <2; i++){
+          balls[i].style.borderWidth = "20px";
+          balls[i].style.backgroundColor = "black";
+          bothEyes[i].style.borderWidth = "5px";
+          bothEyes[i].style.backgroundColor = "white";
+        }
+      }, 200);
+    };
+
+    //I added this function so that the end-user could add an iris of a random color with a click of the mouse.
+    //Early in my course, we were challenged to figure out a way to make a bouncing call change to a random color
+    //everytime it hit a wall. This is how I solved for that challenge and I decided to reuse the code here.
+    //I also added the corresponding border styling to the styles.css document.
+    randomIris= () => {
+      let red = Math.floor(Math.random()*255);
+      let green = Math.floor(Math.random()*255);
+      let blue = Math.floor(Math.random()*255);       
+      let newColor = "rgb(" + red + ", " + green + ", " + blue + ")";
+      for (let i = 0; i < 2; i++) {
+        balls[i].style.borderColor = newColor;
+      }
+    };
+
+    //I added this function to let the user change the eyeshadow.
+    makeover = (n) => {
+      eyeShadow = eyeShadows[n];
+      for (let i = 0; i < 2; i++) {
+        bothEyes[i].style.borderColor = eyeShadow;
+      }
+    }
+
+    //I also added this function so that the user could use the arrow keys instead of some of the buttons.
+
+      document.addEventListener('keyup', function (event) {
+        if (event.key === "ArrowUp"){
+          toggleWatch();
+        }
+        if (event.key === "ArrowLeft"){
+          surpriseEyes();
+        }
+        if (event.key === "ArrowRight"){
+          calmEyes();
+        }
+        if (event.key === "ArrowDown"){
+          blink();
+        }
+      });
